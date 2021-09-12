f3cd63c0ac4822e2f366cca98f192d062d55d222
davalcato <davalcato@yahoo.com>
2021-09-12 09:24:58 -0700

    Created the border of the email text field on the LoginCell by adding the textfield.layer to the emailTextField

--- PlayBook/LoginCell.swift
+++ PlayBook/LoginCell.swift
@@ -34,7 +34,7 @@ class LoginCell: UICollectionViewCell {
             heightConstant: 160)
         logoImageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
         
-        emailTextField.anchor(
+        _ = emailTextField.anchor(
             top: logoImageView.bottomAnchor,
             left: leftAnchor,
             bottom: nil,
@@ -50,48 +50,13 @@ class LoginCell: UICollectionViewCell {
     let emailTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Enter email"
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.borderWidth = 1
         return textField
         
     }()
     
-    lazy var passwordTextField: UITextField = {
-        let tf = UITextField()
-        return tf.textField(withPlaceolder: "Password", isSecureTextEntry: true)
-    }()
     
-    let loginButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("LOG IN", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
-        button.setTitleColor(UIColor.mainBlue(), for: .normal)
-        button.backgroundColor = .white
-        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
-        button.layer.cornerRadius = 5
-        return button
-    }()
-    
-    let dontHaveAccountButton: UIButton = {
-        let button = UIButton(type: .system)
-        let attributedTitle = NSMutableAttributedString(string: "Don't have an account?  ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.white])
-        attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16), NSAttributedString.Key.foregroundColor: UIColor.white]))
-        button.setAttributedTitle(attributedTitle, for: .normal)
-        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
-        return button
-    }()
-    
-    // MARK: - Init
-    
-    @objc func handleLogin() {
-        
-        guard let email = emailTextField.text else { return }
-        guard let password = passwordTextField.text else { return }
-        
-//        logUserIn(withEmail: email, password: password)
-    }
-    
-    @objc func handleShowSignUp() {
-//        navigationController?.pushViewController(SignUpController(), animated: true)
-    }
         
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
